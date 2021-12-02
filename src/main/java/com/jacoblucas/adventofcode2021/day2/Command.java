adb1e9cc9b193a3bc374eecdd51b64d0be79afe4
Jacob Lucas <jacobl@indeed.com>
2021-12-01 21:33:05 -0800

    Day 2

---
+++ src/main/java/com/jacoblucas/adventofcode2021/day2/Command.java
@@ -0,0 +1,7 @@
+package com.jacoblucas.adventofcode2021.day2;
+
+public enum Command {
+    FORWARD,
+    UP,
+    DOWN
+}
---
+++ src/test/java/com/jacoblucas/adventofcode2021/day2/SubmarineTest.java
@@ -0,0 +1,64 @@
+package com.jacoblucas.adventofcode2021.day2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SubmarineTest {
+    private Submarine submarine;
+
+    @Before
+    public void setUp() {
+        submarine = new Submarine();
+    }
+
+    @Test
+    public void testForwardV1() {
+        submarine.execute("forward 5", 1);
+        assertThat(submarine.getPosition(), is(5));
+        assertThat(submarine.getDepth(), is(0));
+    }
+
+    @Test
+    public void testDownV1() {
+        submarine.execute("down 5", 1);
+        assertThat(submarine.getPosition(), is(0));
+        assertThat(submarine.getDepth(), is(5));
+    }
+
+    @Test
+    public void testUpV1() {
+        submarine.execute("down 5", 1);
+        submarine.execute("up 3", 1);
+        assertThat(submarine.getPosition(), is(0));
+        assertThat(submarine.getDepth(), is(2));
+    }
+
+    @Test
+    public void testForwardV2() {
+        submarine.execute("down 1");
+        submarine.execute("forward 5");
+        assertThat(submarine.getPosition(), is(5));
+        assertThat(submarine.getDepth(), is(5));
+        assertThat(submarine.getAim(), is(1));
+    }
+
+    @Test
+    public void testDownV2() {
+        submarine.execute("down 5");
+        assertThat(submarine.getPosition(), is(0));
+        assertThat(submarine.getDepth(), is(0));
+        assertThat(submarine.getAim(), is(5));
+    }
+
+    @Test
+    public void testUpV2() {
+        submarine.execute("down 5");
+        submarine.execute("up 3");
+        assertThat(submarine.getPosition(), is(0));
+        assertThat(submarine.getDepth(), is(0));
+        assertThat(submarine.getAim(), is(2));
+    }
+}

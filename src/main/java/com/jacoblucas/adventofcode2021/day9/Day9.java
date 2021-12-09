b3bc8390382fe51120febf671505dbbc868742f5
Jacob Lucas <jacobl@indeed.com>
2021-12-08 22:13:09 -0800

    Day 9 part 2

--- src/main/java/com/jacoblucas/adventofcode2021/day9/Day9.java
+++ src/main/java/com/jacoblucas/adventofcode2021/day9/Day9.java
@@ -3,8 +3,14 @@ package com.jacoblucas.adventofcode2021.day9;
 import com.jacoblucas.adventofcode2021.utils.InputReader;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Day9 {
 
@@ -19,6 +25,16 @@ public class Day9 {
                 .mapToInt(p -> 1 + heightmap[p.getY()][p.getX()])
                 .sum();
         System.out.println(totalRisk);
+
+        // Part 2
+        final List<Set<Point>> basins = lowPoints.stream()
+                .map(lp -> discoverBasin(lp, heightmap))
+                .sorted(Comparator.comparing(Set::size))
+                .collect(Collectors.toList());
+
+        final int count = basins.size();
+        final int size = basins.get(count - 1).size() * basins.get(count - 2).size() * basins.get(count - 3).size();
+        System.out.println(size);
     }
 
     public static int[][] parse(List<String> input) {
@@ -73,5 +89,43 @@ public class Day9 {
 
     public static int riskLevel(final Point p, final int[][] heightmap) {
         return 1 + heightmap[p.getY()][p.getX()];
+    }
+
+    public static Set<Point> discoverBasin(final Point lowPoint, final int[][] heightmap) {
+        // BFS from the low point until we go off the board or encounter 9s
+        final Queue<Point> queue = new ArrayDeque<>();
+        queue.add(lowPoint);
+        return discoverBasin(heightmap, queue, new HashSet<>());
+    }
+
+    private static Set<Point> discoverBasin(final int[][] heightmap, final Queue<Point> neighbours, final Set<Point> visited) {
+        while (!neighbours.isEmpty()) {
+            final Point p = neighbours.remove();
+            visited.add(p);
+
+            // find all neighbours of p and add to the queue
+            int x = p.getX();
+            int y = p.getY();
+            for (int dy = -1; dy <= 1; dy++) {
+                for (int dx = -1; dx <= 1; dx++) {
+                    int neighbourX = x + dx;
+                    int neighbourY = y + dy;
+                    if (!(Math.abs(dx) + Math.abs(dy) == 1)) {
+                        // Ignore diagonals, and the actual x,y coordinate
+                        continue;
+                    }
+
+                    if (neighbourX >= 0 && neighbourX < heightmap[y].length && neighbourY >= 0 && neighbourY < heightmap.length) {
+                        // found a valid neighbour
+                        final ImmutablePoint neighbour = ImmutablePoint.of(neighbourX, neighbourY);
+                        if (!visited.contains(neighbour) && heightmap[neighbourY][neighbourX] != 9) {
+                            neighbours.add(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+
+        return visited;
     }
 }
--- src/test/java/com/jacoblucas/adventofcode2021/day9/Day9Test.java
+++ src/test/java/com/jacoblucas/adventofcode2021/day9/Day9Test.java
@@ -3,10 +3,12 @@ package com.jacoblucas.adventofcode2021.day9;
 import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
 
 public class Day9Test {
@@ -49,5 +51,64 @@ public class Day9Test {
         assertThat(Day9.riskLevel(ImmutablePoint.of(9, 0), heightmap), is(1));
         assertThat(Day9.riskLevel(ImmutablePoint.of(2, 2), heightmap), is(6));
         assertThat(Day9.riskLevel(ImmutablePoint.of(6, 4), heightmap), is(6));
+    }
+
+    @Test
+    public void testGetBasin1() {
+        final List<Point> expected = ImmutableList.of(
+                ImmutablePoint.of(0,0),
+                ImmutablePoint.of(0,1),
+                ImmutablePoint.of(1,0));
+        assertThat(Day9.discoverBasin(ImmutablePoint.of(1, 0), heightmap), containsInAnyOrder(expected.toArray()));
+    }
+
+    @Test
+    public void testGetBasin2() {
+        final List<Point> expected = new ArrayList<>();
+        expected.add(ImmutablePoint.of(5, 0));
+        expected.add(ImmutablePoint.of(6, 0));
+        expected.add(ImmutablePoint.of(6, 1));
+        expected.add(ImmutablePoint.of(7, 0));
+        expected.add(ImmutablePoint.of(8, 0));
+        expected.add(ImmutablePoint.of(8, 1));
+        expected.add(ImmutablePoint.of(9, 0));
+        expected.add(ImmutablePoint.of(9, 1));
+        expected.add(ImmutablePoint.of(9, 2));
+        assertThat(Day9.discoverBasin(ImmutablePoint.of(9, 0), heightmap), containsInAnyOrder(expected.toArray()));
+    }
+
+    @Test
+    public void testGetBasin3() {
+        final List<Point> expected = new ArrayList<>();
+        expected.add(ImmutablePoint.of(2, 1));
+        expected.add(ImmutablePoint.of(3, 1));
+        expected.add(ImmutablePoint.of(4, 1));
+        expected.add(ImmutablePoint.of(1, 2));
+        expected.add(ImmutablePoint.of(2, 2));
+        expected.add(ImmutablePoint.of(3, 2));
+        expected.add(ImmutablePoint.of(4, 2));
+        expected.add(ImmutablePoint.of(5, 2));
+        expected.add(ImmutablePoint.of(0, 3));
+        expected.add(ImmutablePoint.of(1, 3));
+        expected.add(ImmutablePoint.of(2, 3));
+        expected.add(ImmutablePoint.of(3, 3));
+        expected.add(ImmutablePoint.of(4, 3));
+        expected.add(ImmutablePoint.of(1, 4));
+        assertThat(Day9.discoverBasin(ImmutablePoint.of(2, 2), heightmap), containsInAnyOrder(expected.toArray()));
+    }
+
+    @Test
+    public void testGetBasin4() {
+        final List<Point> expected = new ArrayList<>();
+        expected.add(ImmutablePoint.of(7, 2));
+        expected.add(ImmutablePoint.of(6, 3));
+        expected.add(ImmutablePoint.of(7, 3));
+        expected.add(ImmutablePoint.of(8, 3));
+        expected.add(ImmutablePoint.of(5, 4));
+        expected.add(ImmutablePoint.of(6, 4));
+        expected.add(ImmutablePoint.of(7, 4));
+        expected.add(ImmutablePoint.of(8, 4));
+        expected.add(ImmutablePoint.of(9, 4));
+        assertThat(Day9.discoverBasin(ImmutablePoint.of(6, 4), heightmap), containsInAnyOrder(expected.toArray()));
     }
 }
